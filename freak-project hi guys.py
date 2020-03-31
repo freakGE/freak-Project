76663c9c6d27d3e1cdc6cd9453512811eba2ffc0
freak <52050303+freakGE@users.noreply.github.com>
2020-03-31 18:17:04 +0400

    Add files via upload

---
+++ freak-project hi guys.py
@@ -0,0 +1,23 @@
+#freal project
+
+def wellcome_guys():
+    name = "Saba"
+    my_skill = "begginer"
+
+
+    your_name = input("What's your name?: ")
+    your_skill = input("1. Begginer 2. Professional 3. Normal (in the middle)\nSkill: ") 
+    if your_skill == "1":
+        print("Me too))\n")
+    elif your_skill == "2":
+        print("Cool, I cant guide u cuz u are better than me but ty for reading..\n")
+    else:
+        print("")    
+    
+
+    print(f"Hi {your_name} my name is {name} and I want improve my skills")
+    print(f"I am {my_skill} so i rly want be a good xD")
+    print(f"If u want contact me\nMail: esebua154@gmail.com")
+
+
+wellcome_guys()
